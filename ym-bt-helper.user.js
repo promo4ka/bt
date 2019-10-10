38cae246f53d41dec44997a0b19f20ddc411d9c1
promo4ka <moroz-yra@yandex.ru>
2019-10-10 13:30:22 +0300

    Add counter opened report + settings

--- ym-bt-helper.user.js
+++ ym-bt-helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Баг-трекер плюшки
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Меню с избранными продуктами на весь сайт
 // @author       i_mrz
 // @match        https://vk.com/*
@@ -15,12 +15,22 @@
 (function() {
 	'use strict';
 
+	const settings = {
+		showCountOpenedReports: false // Показывать ли количество открытых отчетов
+	};
+
 	const observer = new MutationObserver((mutations) => {
 		mutations.forEach((mutation) => {
 			if (mutation.target.getAttribute('id') !== "wrap3") return;
 			initBTUtils();
 		});
 	});
+
+	const formatNumber = (number) => {
+		if (number === null) { return 0; }
+
+		return `${number}`.replace(/(\d)(?=(\d{3})+$)/g, '$1 ');
+	};
 
 	const styles = `
 			.ym-bt-menu {
@@ -55,9 +65,57 @@
 		return products;
 	}
 
+	const getCountOpenedReports = async (product_id) => {
+		return await new Promise((resolve, reject) => {
+			var _data = new FormData();
+			_data.append("load", 1);
+			_data.append("al", 1);
+			_data.append("original", 1);
+			_data.append("product", product_id);
+			_data.append("status", 0);
+			_data.append("wishes", 1);
+
+			const req = new XMLHttpRequest();
+			req.open('POST', 'https://vk.com/bugs', true);
+			req.onreadystatechange = () => {
+				if (req.readyState != 4) return;
+
+				if(req.status !== 200) { return reject("I/O Error", req.status); }
+
+				let _response = req.response;
+				try {
+					_response = JSON.parse(req.response.replace(/^.+?\{/,"{"));
+					_response = _response.payload[1];
+				} catch(e) {}
+
+				let count = false;
+				if (_response[0]) {
+					const response = _response[0].replace('Склеить отчёты', '');
+					const left = `${response}`.indexOf("Найден");
+					const right = `${response}`.indexOf("отчёт")+5;
+					const center = `${response}`.slice(left, right);
+					count = center.split(" ")[1] || 0;
+				}
+
+				if(count !== false) {
+					resolve(count);
+				} else if(_response.error && _response.error.error_code == 6) {
+					getCountOpenedReports(product_id).then(resolve, reject);
+				} else {
+					reject(_response);
+				}
+			};
+
+			req.send(_data);
+		});
+	}
+
 	const initMenu = () => {
 		const products = getProducts();
-		const productList = products.map((item, index) => `<a href="${item.link}"><li>${item.name}</li></a>`).join('');
+		const productList = products.map((item, index) => {
+			const opened = settings.showCountOpenedReports ? ` (${item.count})` : '';
+			return `<a href="${item.link}"><li>${item.name}${opened}</li></a>`
+		}).join('');
 
 		if (productList) {
 			const btOldMenu = document.querySelector('.ym-bt-menu');
@@ -105,6 +163,31 @@
 		}
 	}
 
+	const updateProducts = (add = false) => {
+		if (!settings.showCountOpenedReports) return;
+
+		const products = getProducts();
+		const lastUpdate = localStorage.getItem('ymLastUpdate');
+		const timestamp = Math.round(+new Date()/1000);
+
+		if (!lastUpdate || (lastUpdate+3600) < timestamp || add) {
+			products.forEach((item, index) => {
+				getCountOpenedReports(item.id).then((r) => {
+					const updateProducts = getProducts();
+					updateProducts[index].count = formatNumber(r);
+
+					localStorage.setItem('ymBtProducts', JSON.stringify(updateProducts));
+
+					if (index === products.length-1) {
+						setTimeout(initMenu, 333);
+					}
+				})
+			});
+
+			localStorage.setItem('ymLastUpdate', timestamp);
+		}
+	}
+
 	document.addEventListener('click', function(event) {
 		const element = event.target;
 
@@ -121,7 +204,8 @@
 				products.push({
 					id: pId,
 					name: pName,
-					link: pLink
+					link: pLink,
+					count: 0
 				});
 			} else {
 				element.innerText = '+';
@@ -131,16 +215,28 @@
 			}
 
 			localStorage.setItem('ymBtProducts', JSON.stringify(products));
+			updateProducts(true);
 			initMenu();
 		}
 	});
 
-	const style = document.createElement('style');
-		style.innerHTML = styles;
+	const init = () => {
+		const style = document.createElement('style');
+			style.innerHTML = styles;
 
-	document.head.append(style);
+		document.head.append(style);
 
-	initBTUtils();
-	initMenu();
+		updateProducts();
+		initBTUtils();
+		initMenu();
+	}
+
+	init();
+	// window.addEventListener("load", init);
 
 })();
+
+/**
+ * LOG CHANGES
+ * v1.3 - Добавлен вывод количества открытых отчетов
+ */
